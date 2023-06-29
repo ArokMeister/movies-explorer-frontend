b01239a7dc2a3c33cfcabc5880a1fa47c4123891
Denis Shiryaev/ <arokmeister@yandex.ru/>
2023-06-29 23:58:13 +0300

    fix: заменен домен бэка

--- src/utils/MainApi.js
+++ src/utils/MainApi.js
@@ -1,5 +1,4 @@
-// export const BASE_URL = 'https://api.movie-explorer.nomoredomains.rocks';
-export const BASE_URL = 'http://localhost:3000';
+export const BASE_URL = 'https://api.movie-explorer.nomoredomains.rocks';
 
 function makeRequest(url, method, body) {
   const headers = {
